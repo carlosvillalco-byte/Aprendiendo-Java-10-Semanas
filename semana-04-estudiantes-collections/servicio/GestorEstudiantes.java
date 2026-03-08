73a69d8f1e4cc5b4f1a4d55ae2dd4cdb57f7182a
Carlos A. Villca Alcocer <carlos.villca_alcocer@sistemas.edu.bo>
2026-03-07 20:26:42 -0400

    feat:crear clase GestorEstudiantes

--- semana-04-estudiantes-collections/servicio/GestorEstudiantes.java
+++ semana-04-estudiantes-collections/servicio/GestorEstudiantes.java
@@ -1,41 +1,10 @@
 package servicio;
+
 import modelo.Estudiante;
-import java.util.*;
+import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.ArrayList;
 
 public class GestorEstudiantes {
 
-    private HashMap<String, Estudiante> mapaEstudiantes;
-    private TreeSet<Estudiante> ranking;
-    private ArrayList<Estudiante> lista;
-
-    public GestorEstudiantes() {
-        mapaEstudiantes = new HashMap<>();
-        ranking = new TreeSet<>();
-        lista = new ArrayList<>();
-    }
-
-    public boolean agregarEstudiante(Estudiante e) {
-
-        if (mapaEstudiantes.containsKey(e.getCarnet())) {
-            return false;
-        }
-
-        mapaEstudiantes.put(e.getCarnet(), e);
-        ranking.add(e);
-        lista.add(e);
-
-        return true;
-    }
-
-    public Estudiante buscarPorCarnet(String carnet) {
-        return mapaEstudiantes.get(carnet);
-    }
-
-    public List<Estudiante> listarTodos() {
-        return lista;
-    }
-
-    public Set<Estudiante> obtenerRanking() {
-        return ranking;
-    }
 }
