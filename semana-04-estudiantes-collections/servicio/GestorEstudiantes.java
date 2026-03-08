6fe1148b5968cbcea702a7473dd4a026c0be17ea
Carlos A. Villca Alcocer <carlos.villca_alcocer@sistemas.edu.bo>
2026-03-07 20:31:23 -0400

    feat:inicializar colecciones

--- semana-04-estudiantes-collections/servicio/GestorEstudiantes.java
+++ semana-04-estudiantes-collections/servicio/GestorEstudiantes.java
@@ -6,8 +6,15 @@ import java.util.TreeSet;
 import java.util.ArrayList;
 
 public class GestorEstudiantes {
-    private HashMap<String, Estudiante> porCarnet;
+     private HashMap<String, Estudiante> porCarnet;
     private TreeSet<Estudiante> ranking;
     private ArrayList<String> historial;
+
+    public GestorEstudiantes() {
+
+        porCarnet = new HashMap<>();
+        ranking = new TreeSet<>();
+        historial = new ArrayList<>();
+    }
 
 }
