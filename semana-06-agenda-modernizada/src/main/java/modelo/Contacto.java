e66a40b6c1bd6d3afde2a07b7bf565d2a06ab018
Carlos A. Villca Alcocer <carlos.villca_alcocer@sistemas.edu.bo>
2026-03-21 14:15:55 -0400

    feat:se corrige Contacto.java

--- semana-06-agenda-modernizada/src/main/java/modelo/Contacto.java
+++ semana-06-agenda-modernizada/src/main/java/modelo/Contacto.java
@@ -1,7 +1,5 @@
 package modelo;
 
-import exception.DatoInvalidoException;
-
 public class Contacto {
 
     private String id;
@@ -9,16 +7,14 @@ public class Contacto {
     private String telefono;
     private String email;
     private String direccion;
-    private String categoria; 
 
-    public Contacto(String id, String nombre, String telefono, String email, String direccion, String categoria) {
+    public Contacto(String id, String nombre, String telefono, String email, String direccion) {
 
         setId(id);
         setNombre(nombre);
         setTelefono(telefono);
         setEmail(email);
         setDireccion(direccion);
-        this.categoria = categoria; 
     }
 
     public String getId() { return id; }
@@ -26,12 +22,11 @@ public class Contacto {
     public String getTelefono() { return telefono; }
     public String getEmail() { return email; }
     public String getDireccion() { return direccion; }
-    public String getCategoria() { return categoria; } 
 
     public void setId(String id) {
 
         if(id == null || id.trim().isEmpty())
-            throw new DatoInvalidoException("id","no puede estar vacío");
+            throw new IllegalArgumentException("id no puede estar vacío");
 
         this.id = id;
     }
@@ -39,7 +34,7 @@ public class Contacto {
     public void setNombre(String nombre) {
 
         if(nombre == null || nombre.trim().isEmpty())
-            throw new DatoInvalidoException("nombre","no puede estar vacío");
+            throw new IllegalArgumentException("nombre no puede estar vacío");
 
         this.nombre = nombre;
     }
@@ -47,13 +42,13 @@ public class Contacto {
     public void setTelefono(String telefono) {
 
         if(telefono == null || telefono.trim().isEmpty())
-            throw new DatoInvalidoException("telefono","no puede estar vacío");
+            throw new IllegalArgumentException("telefono no puede estar vacío");
 
         if(!telefono.matches("\\d+"))
-            throw new DatoInvalidoException("telefono","solo números");
+            throw new IllegalArgumentException("telefono solo debe contener números");
 
         if(telefono.length() < 7 || telefono.length() > 8)
-            throw new DatoInvalidoException("telefono","debe tener 7 u 8 dígitos");
+            throw new IllegalArgumentException("telefono debe tener 7 u 8 dígitos");
 
         this.telefono = telefono;
     }
@@ -63,20 +58,21 @@ public class Contacto {
         if(email != null && !email.trim().isEmpty()){
 
             if(!email.matches("^[A-Za-z0-9+_.-]+@(.+)$"))
-                throw new DatoInvalidoException("email","formato de email inválido");
+                throw new IllegalArgumentException("formato de email inválido");
         }
 
         this.email = email;
     }
 
     public void setDireccion(String direccion) {
+
         this.direccion = direccion;
     }
 
     @Override
     public String toString(){
 
-        return String.format("%-5s | %-15s | %-9s | %s | %s",
-                id,nombre,telefono,email,categoria);
+        return String.format("%-5s | %-15s | %-9s | %s",
+                id,nombre,telefono,email);
     }
 }
