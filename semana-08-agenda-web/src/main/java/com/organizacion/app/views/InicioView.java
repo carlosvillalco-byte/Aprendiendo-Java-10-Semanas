4d40e53f480027edd17e9dc0f540fa23d1525d4b
Carlos A. Villca Alcocer <carlos.villca_alcocer@sistemas.edu.bo>
2026-04-10 19:16:49 -0400

    feat:InicioView con H2, Paragraph y H3

--- semana-08-agenda-web/src/main/java/com/organizacion/app/views/InicioView.java
+++ semana-08-agenda-web/src/main/java/com/organizacion/app/views/InicioView.java
@@ -1,32 +1,33 @@
-package com.organizacion.agenda.views;
+package com.organizacion.app.views;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H1;
+import com.organizacion.app.ui.MainLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.router.Route;
 
-@Route("")
+@Route(value = "", layout = MainLayout.class)
 public class InicioView extends VerticalLayout {
 
     public InicioView() {
+        setSizeFull();
         setPadding(true);
-        setSpacing(true);
 
-        H1 titulo = new H1("Agenda de Contactos");
-        H2 subtitulo = new H2("Tu agenda personal");
+        VerticalLayout contenido = new VerticalLayout();
+        contenido.setPadding(false);
+        H2 titulo=new H2 ("Bienvenido a tu Agenda");
+        Paragraph descripcion = new Paragraph("Guarda y organiza tus Contactos en un solo Lugar");
+        H3 instruccion =new H3("Usa el menu Superior para Navegar");
 
-        Paragraph descripcion = new Paragraph(
-                "Guarda, edita y busca tus contactos en un solo lugar."
-        );
+        contenido.add(titulo,descripcion,instruccion);
+        Div footer =new Div(new Span("Agenda de Contactos v1.0"));
+        footer.setWidthFull();
 
-        Button verContactos = new Button("Ver contactos");
-
-        verContactos.addClickListener(e ->
-                getUI().ifPresent(ui -> ui.navigate("contactos"))
-        );
-
-        add(titulo, subtitulo, descripcion, verContactos);
+        add(contenido,footer);
+        expand(contenido);
     }
 }
